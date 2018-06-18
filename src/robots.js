e91a23b6e3ac32d03dd53e80667fe6600c737c78
Randy Davis <randy_davis@outlook.com>
2018-06-17 22:49:59 -0500

    initialize repo w/Robofriends app files

---
+++ src/robots.js
@@ -0,0 +1,50 @@
+export const robots = [
+    {
+        id:1,
+        name: 'Leanne Graham',
+        username: 'Bret',
+        email: 'Sincere@april.biz'
+    },
+    {
+        id:2,
+        name: 'Anne Malone',
+        username: 'Annie',
+        email: 'dontanneme@myown.biz'
+    },
+    {
+        id:3,
+        name: 'Graham Strickland',
+        username: 'Graham',
+        email: 'oggraham@test.com'
+    },
+    {
+        id:4,
+        name: 'David Davidson',
+        username: 'DoubleD',
+        email: 'david@test.com'
+    },
+    {
+        id:5,
+        name: 'Sarah Smith',
+        username: 'SarahS',
+        email: 'Sarah@test.com'
+    },
+    {
+        id:6,
+        name: 'Lina Stevens',
+        username: 'LadyLina',
+        email: 'Lina@test.com'
+    },
+    {
+        id:7,
+        name: 'Lee Bruce',
+        username: 'Lee',
+        email: 'BruceLee@test.com'
+    },
+    {
+        id:8,
+        name: 'Krista Davis',
+        username: 'KKDavi',
+        email: 'Krista@test.com'
+    },
+]
